70f56c4c8cff0b626cf81f0136cf598472de3a89
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-22 14:00:13 -0600

    ejercicio3

---
+++ actividad4/ejercicio3.py
@@ -0,0 +1,8 @@
+while True:
+    velocidad = float(input("Ingrese la velocidad en km/h: "))
+
+    if velocidad > 120:
+        print("¡Reduzca la velocidad!")
+    else:
+        print("Velocidad dentro del límite.")
+        break
