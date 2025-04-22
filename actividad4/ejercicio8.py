a4141ba5984a4d4fd89ecf529f086ab0869b3b56
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-22 15:08:17 -0600

    ejercicio8

---
+++ actividad4/ejercicio8.py
@@ -0,0 +1,7 @@
+usuario = input("Ingrese nombre de usuario: ")
+clave = input("Ingrese la clave: ")
+
+if usuario == "alondra" and clave == "alondramayen":
+    print("Acceso permitido")
+else:
+    print("Acceso denegado")
