fe038dde7d76d7836e049b709c666a1435e4c19f
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-22 08:21:43 -0600

    inventadio_disponible

--- actividad3/ejercicio2.py
+++ actividad3/ejercicio2.py
@@ -9,8 +9,13 @@
 #Mostrar los productos recibidos y vendidos
 #Mostrar el inventario final disponible
 
-cantidad_inicial = int(input("Ingrese la cantidad inicial en inventario"))
-cantidad_recibidos = int(input("Ingrese la cantidad de productos recibidos"))
-cantidad_vendidos = int(input("Ingrese la cantidad de productos vendidos"))
+cantidad_inicial = int(input("Ingrese la cantidad inicial en inventario: "))
+cantidad_recibidos = int(input("Ingrese la cantidad de productos recibidos: "))
+cantidad_vendidos = int(input("Ingrese la cantidad de productos vendidos: "))
 total1 = cantidad_inicial + cantidad_recibidos
 total2 = total1 - cantidad_vendidos
+
+print(f"""El inventario inicial es de: {cantidad_inicial}
+La cantidad de productos recibidos de: {cantidad_recibidos}
+La cantidad de productos vendidos es de:  {cantidad_vendidos}
+El inventatio final es de: {total2} """)
