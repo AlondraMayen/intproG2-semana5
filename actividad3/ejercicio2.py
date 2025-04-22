4a1e39e0b45a98823ed9113c1f62a5b67ad36fe7
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-22 08:07:17 -0600

    inventario actual

---
+++ actividad3/ejercicio2.py
@@ -0,0 +1,16 @@
+#control de inventario de un producto
+#Ingresar la cantidad inicial en inventario
+#Ingresar la cantidad de productos recibidos
+#Ingresar la cantidad de productos vendidos
+#Sumar los productos recibidos al inventario inicial
+#Restar los productos vendidos del total anterior
+#Guardar el resultado como inventario actual
+#Mostrar el inventario inicial
+#Mostrar los productos recibidos y vendidos
+#Mostrar el inventario final disponible
+
+cantidad_inicial = int(input("Ingrese la cantidad inicial en inventario"))
+cantidad_recibidos = int(input("Ingrese la cantidad de productos recibidos"))
+cantidad_vendidos = int(input("Ingrese la cantidad de productos vendidos"))
+total1 = cantidad_inicial + cantidad_recibidos
+total2 = total1 - cantidad_vendidos
