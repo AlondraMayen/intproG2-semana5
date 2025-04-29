520309f067f8720ffa095569e6b68d8c1095dcdc
Alondra <asmayen@uamv.edu.ni>
2025-04-29 07:02:55 -0600

    Add files via upload

---
+++ actividad5/ejercicio3.py
@@ -0,0 +1,21 @@
+dia = int(input("Ingrese el dia de la semana de forma numerica (1-7): "))
+while dia !=0:
+    if dia==1:
+        print("lunes")
+    elif dia==2:
+        print ("martes")
+    elif dia==3:
+        print ("miercoles")
+    elif dia==4:
+        print ("jueves")
+    elif dia==5:
+        print ("viernes")
+    elif dia==6:
+        print ("sabado")
+    elif dia==7:
+        print ("domingo")
+    else:
+        print("Error, el numero digitado no es valido")
+
+print("Preciona 0 para salir")
+dia = int(input("Ingrese el dia de la semana de forma numerica(1-7): "))
